3bceab095900e7fb0b6d7c2f0ad3d115c989ac72
ZeTryall <zeetryall@gmail.com>
2018-02-02 22:59:01 +0100

    Basic Player movement push

---
+++ PlayerScripts/Joystick&Keyboard_Movement.cs
@@ -0,0 +1,11 @@
+﻿	//Move the player in the X and Y axis. for Keyboard and Gamepad.
+    public void SetPlayerDirection(){
+        curSpeed = walkSpeed;
+        maxSpeed = curSpeed;
+
+        Rb2d.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * curSpeed, 0.8f),
+                           Mathf.Lerp(0, Input.GetAxis("Vertical") * curSpeed, 0.8f));
+    }
+  
+
+
---
+++ PlayerScripts/Joysticks_Rotation.cs
@@ -0,0 +1,17 @@
+﻿	//Move the player with the Right Joystick. With a little Smooth effect.
+    public void SetPlayerRotationGamepad(){
+
+        float JoystickX = Input.GetAxis("JoystickX");
+        float JoystickY = Input.GetAxis("JoystickY");
+
+        if ((JoystickX != 0) || (JoystickY != 0F)) {                   //If Joysticks are used ; Do some complicated things.
+        float heading = Mathf.Atan2(JoystickX, JoystickY);              
+        Quaternion TowardThatPosition = Quaternion.Euler(0.0F, 0.0F, heading * Mathf.Rad2Deg);
+        Quaternion FinalLerpedQuaternion = Quaternion.Lerp(Rb2d.transform.rotation, TowardThatPosition, 0.25F);
+
+        transform.rotation = FinalLerpedQuaternion;
+        }
+    }
+  
+
+
